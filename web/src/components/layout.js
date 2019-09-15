8d06d1f9aaffffac8dd5dfd2f118278515020faa
kevin power <kevin.power@mobelux.com>
2019-09-15 19:46:52 -0400

    deploy test with semantic ui

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,11 +1,48 @@
 import React from 'react'
 import Header from './header'
+import Helmet from 'react-helmet'
+import {
+  Container,
+  Dropdown,
+  Menu,
+ß} from 'semantic-ui-react'
 
 import '../styles/layout.css'
 import styles from './layout.module.css'
 
 const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => (
   <>
+    <Helmet>
+      <link href="https://fonts.googleapis.com/css?family=Slackey&display=swap" rel="stylesheet" />
+      <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+    </Helmet>
+    <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item as='a' header>
+          {siteTitle}
+        </Menu.Item>
+        <Menu.Item as='a'>Home</Menu.Item>
+
+        <Dropdown item simple text='Dropdown'>
+          <Dropdown.Menu>
+            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Header>Header Item</Dropdown.Header>
+            <Dropdown.Item>
+              <i className='dropdown icon' />
+              <span className='text'>Submenu</span>
+              <Dropdown.Menu>
+                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Item>List Item</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown.Item>
+            <Dropdown.Item>List Item</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Container>
+    </Menu>
+  
     <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
     <div className={styles.content}>{children}</div>
     <footer className={styles.footer}>
