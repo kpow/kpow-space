0bbaf26d4ec33ff00c54d618398dd499a3e24edf
kevin power <kevin.power@mobelux.com>
2019-09-15 20:27:05 -0400

    header works and added skull svg

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -2,21 +2,64 @@ import {Link} from 'gatsby'
 import React from 'react'
 import Icon from './icon'
 import {cn} from '../lib/helpers'
+import {Container, Menu} from 'semantic-ui-react'
 
-import styles from './header.module.css'
+import HeaderStyles from './header.module.css'
+import styles from './project-preview.module.css'
+
+import Skull from '../static/skull.svg';
 
 const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
   <div className={styles.root}>
-    <div className={styles.wrapper}>
-      <div className={styles.branding}>
-        <Link to='/'>{siteTitle}</Link>
-      </div>
 
-      <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
+    <Menu fixed="top" size="large" inverted compact>
+      <Container>
+        <Menu.Item style={{ padding: '0' }} as={Link} to="/">
+          <h1 className="site-logo">
+            <img
+              height="50"
+              style={{
+                display: 'inline-block',
+                paddingTop: '3px',
+                paddingBottom: '5px',
+                verticalAlign: 'middle'
+              }}
+              src={Skull}
+            />
+            <span
+              style={{
+                display: 'inline-block',
+                paddingRight: '5px',
+                paddingTop: '5px',
+                height: '50px',
+                verticalAlign: 'middle',
+                fontFamily:'Slackey'
+              }}
+            >
+              kpow
+            </span>
+          </h1>
+        </Menu.Item>
+        <Menu.Item position='right' as={Link} to='/allstars'>
+          allstars
+        </Menu.Item>
+        <Menu.Item as={Link} to='/allbooks'>
+          allbooks
+        </Menu.Item>
+        <Menu.Item>
+          <a href='https://instagram.com/kpow_musicgram'>insta</a>
+        </Menu.Item>
+      </Container>
+    </Menu>
+  
+
+    <div className={HeaderStyles.wrapper}>
+
+      <button className={HeaderStyles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
         <Icon symbol='hamburger' />
       </button>
 
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
+      <nav className={cn(styles.nav, showNav && HeaderStyles.showNav)}>
         <ul>
           <li>
             <Link to='/archive/'>Archive</Link>
--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import Header from './header'
 import Helmet from 'react-helmet'
-import {
-  Container,
-  Dropdown,
-  Menu,
-ß} from 'semantic-ui-react'
+
+
 
 import '../styles/layout.css'
-import styles from './layout.module.css'
+import LayoutStyles from './layout.module.css'
 
 const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => (
   <>
@@ -16,37 +13,11 @@ const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => (
       <link href="https://fonts.googleapis.com/css?family=Slackey&display=swap" rel="stylesheet" />
       <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
     </Helmet>
-    <Menu fixed='top' inverted>
-      <Container>
-        <Menu.Item as='a' header>
-          {siteTitle}
-        </Menu.Item>
-        <Menu.Item as='a'>Home</Menu.Item>
 
-        <Dropdown item simple text='Dropdown'>
-          <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
-            <Dropdown.Item>
-              <i className='dropdown icon' />
-              <span className='text'>Submenu</span>
-              <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Container>
-    </Menu>
-  
     <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
-    <div className={styles.content}>{children}</div>
-    <footer className={styles.footer}>
-      
+    <div className={LayoutStyles.content}>{children}</div>
+    <footer className={LayoutStyles.footer}>
+
     </footer>
   </>
 )
