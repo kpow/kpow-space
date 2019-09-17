7fa90873100331098fa0cc3c7ac10b3842df6870
kevin power <kevin.power@mobelux.com>
2019-09-16 22:19:04 -0400

    build fail, removed xmlhttp

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -63,37 +63,42 @@ export const query = graphql`
 
 
 function getJSON(url) {
-  var xhr = new XMLHttpRequest();
-  return new Promise((resolve, reject) => {
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject(xhr.responseText);
-        }
-      }
-    };
-    xhr.open("GET", url);
-    xhr.send();
-  });
+
+  fetch(url)
+      .then(response => response.json())
+      .then(data => console.log(data));
+
+  // var xhr = new XMLHttpRequest();
+  // return new Promise((resolve, reject) => {
+  //   xhr.onreadystatechange = function() {
+  //     if (xhr.readyState === 4) {
+  //       if (xhr.status === 200) {
+  //         resolve(JSON.parse(xhr.responseText));
+  //       } else {
+  //         reject(xhr.responseText);
+  //       }
+  //     }
+  //   };
+  //   xhr.open("GET", url);
+  //   xhr.send();
+  // });
 }
 
 function getXML(url) {
-  var xhr = new XMLHttpRequest();
-  return new Promise((resolve, reject) => {
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          resolve(xhr.responseText);
-        } else {
-          reject(xhr.responseText);
-        }
-      }
-    };
-    xhr.open("GET", url);
-    xhr.send();
-  });
+  // var xhr = new XMLHttpRequest();
+  // return new Promise((resolve, reject) => {
+  //   xhr.onreadystatechange = function() {
+  //     if (xhr.readyState === 4) {
+  //       if (xhr.status === 200) {
+  //         resolve(xhr.responseText);
+  //       } else {
+  //         reject(xhr.responseText);
+  //       }
+  //     }
+  //   };
+  //   xhr.open("GET", url);
+  //   xhr.send();
+  // });
 }
 
 const getStarsData = (page = 1) => {
