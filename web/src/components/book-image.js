4fc6db664e5bbdd02497f4dc5ccfe74019d91f3f
kevin power <kevin.power@mobelux.com>
2019-09-22 21:11:47 -0400

    better pattern for the hacky labels

---
+++ web/src/components/book-image.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Image, Label } from 'semantic-ui-react'
+
+const BookImage = (props) => (
+  <Image
+    floated='right'
+    size='tiny'
+    src={props.imageUrl}
+    label={{
+      color: props.labelColor,
+      content:props.shelf,
+      ribbon: 'right',
+    }}
+  />
+
+)
+
+export default BookImage
--- web/src/components/book-preview.js
+++ web/src/components/book-preview.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {cn, buildImageObj} from '../lib/helpers'
-import {imageUrlFor} from '../lib/image-url'
-import { Card, Icon, Feed, Image, Label, Rating } from 'semantic-ui-react'
+import BookImage from '../components/book-image'
+import { Card, Rating } from 'semantic-ui-react'
 
 import styles from './book-preview.module.css'
 
@@ -13,83 +13,62 @@ function BookPreview (props) {
     shelf = 'unknown'
   }
 
-  
   return (
- <div>
+  <div>
 
-<Card as='a' 
+    <Card as='a' 
       href={props.book.link._text} 
       fluid 
       target="_new"
       style={{minHeight:'150px', maxHeight:'150px', marginBottom:'20px'}}
->
-<Card.Content>
+    >
+    <Card.Content>
 
-{shelf == 'currently-reading' && ( 
-  <Image
-    floated='right'
-    size='tiny'
-    src={props.book.image_url._text}
-    label={{
-      color: 'purple',
-      content:shelf,
-      ribbon: 'right',
-    }}
-  />
-)}
+    {shelf == 'currently-reading' && ( 
+      <BookImage 
+        labelColor='purple' 
+        imageUrl={props.book.image_url._text}
+        shelf={shelf} 
+      />   
+    )}
 
-{shelf == 'read' && (
-  <Image
-    floated='right'
-    size='tiny'
-    src={props.book.image_url._text}
-    label={{
-      color: 'teal',
-      content:shelf,
-      ribbon: 'right',
-    }}
-  />
-)}
+    {shelf == 'read' && (
+      <BookImage 
+        labelColor='teal' 
+        imageUrl={props.book.image_url._text}
+        shelf={shelf} 
+      />   
+    )}
 
-{shelf == 'to-read' && (
-  <Image
-    floated='right'
-    size='tiny'
-    src={props.book.image_url._text}
-    label={{
-      color: 'blue',
-      content:shelf,
-      ribbon: 'right',
-    }}
-  />
-)}
+    {shelf == 'to-read' && (
+      <BookImage 
+        labelColor='blue' 
+        imageUrl={props.book.image_url._text}
+        shelf={shelf} 
+      />   
+    )}
 
-{shelf == 'unknown' && (
-  <Image
-    floated='right'
-    size='tiny'
-    src={props.book.image_url._text}
-    label={{
-      color: 'red',
-      content:shelf,
-      ribbon: 'right',
-    }}
-  />
-)}
- 
-  <Card.Header>{props.book.title_without_series._text}</Card.Header>
-  <Card.Meta><strong>Author: </strong>{props.book.authors.author.name._text}</Card.Meta>
-  <Card.Meta><strong>Published: </strong>{props.book.published._text}</Card.Meta>
-  <Card.Meta><strong>Avg. Rating: </strong>
-    <Rating defaultRating={props.book.average_rating._text} maxRating={5} disabled />
-  </Card.Meta>
-  <Card.Meta><strong>My Rating: </strong>
-    <Rating defaultRating={props.rating._text} maxRating={5} disabled />
-  </Card.Meta>
-</Card.Content>
-</Card>
+    {shelf == 'unknown' && (
+      <BookImage 
+        labelColor='red' 
+        imageUrl={props.book.image_url._text}
+        shelf={shelf} 
+      />   
+    )}
+    
+      <Card.Header>{props.book.title_without_series._text}</Card.Header>
+      <Card.Meta><strong>Author: </strong>{props.book.authors.author.name._text}</Card.Meta>
+      <Card.Meta><strong>Published: </strong>{props.book.published._text}</Card.Meta>
+      <Card.Meta><strong>Avg. Rating: </strong>
+        <Rating defaultRating={props.book.average_rating._text} maxRating={5} disabled />
+      </Card.Meta>
+      <Card.Meta><strong>My Rating: </strong>
+        <Rating defaultRating={props.rating._text} maxRating={5} disabled />
+      </Card.Meta>
+    </Card.Content>
+    </Card>
 
-</div>
+  </div>
 
   )
 }
